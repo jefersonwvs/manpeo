040c50e8ac0643102d0a054aa1951f599c58e25b
Jeferson Willian Vieira Silva <jefersonwvs@gmail.com>
2023-01-09 20:18:26 -0400

    project creation

---
+++ src/main/java/com/jefersonwvs/manpeo/ManpeoApplication.java
@@ -0,0 +1,13 @@
+package com.jefersonwvs.manpeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManpeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManpeoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jefersonwvs/manpeo/ManpeoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jefersonwvs.manpeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManpeoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
