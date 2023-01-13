ad5f1253fa2cec6d550eaddb64a4717aece9e2cf
Jeferson Willian Vieira Silva <jefersonwvs@gmail.com>
2023-01-13 09:00:47 -0400

    test: utilitarian method to create address dto

--- src/test/java/com/jefersonwvs/manpeo/utils/Factory.java
+++ src/test/java/com/jefersonwvs/manpeo/utils/Factory.java
@@ -1,5 +1,6 @@
 package com.jefersonwvs.manpeo.utils;
 
+import com.jefersonwvs.manpeo.dtos.AddressDTO;
 import com.jefersonwvs.manpeo.dtos.PersonDTO;
 import com.jefersonwvs.manpeo.entities.Address;
 import com.jefersonwvs.manpeo.entities.Person;
@@ -17,8 +18,11 @@ public class Factory {
 	}
 
 	public static Address createAddress() {
-		return new Address(1L, "Rua Afonso Pena", "362", "10024-934", "Campo Grande", false,
-											 createPerson());
+		return new Address(1L, "Rua Afonso Pena", "362", "10024-934", "Campo Grande", false, null);
+	}
+
+	public static AddressDTO createAddressDTO() {
+		return new AddressDTO(null, "Rua Afonso Pena", "362", "10024-934", "Campo Grande", false);
 	}
 
 }
