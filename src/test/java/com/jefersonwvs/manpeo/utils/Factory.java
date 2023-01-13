6adae101719d70cd1ba060cfe203c976196931ed
Jeferson Willian Vieira Silva <jefersonwvs@gmail.com>
2023-01-12 20:51:04 -0400

    test: utilitarian method to create person dto

--- src/test/java/com/jefersonwvs/manpeo/utils/Factory.java
+++ src/test/java/com/jefersonwvs/manpeo/utils/Factory.java
@@ -1,5 +1,6 @@
 package com.jefersonwvs.manpeo.utils;
 
+import com.jefersonwvs.manpeo.dtos.PersonDTO;
 import com.jefersonwvs.manpeo.entities.Person;
 
 import java.time.LocalDate;
@@ -8,6 +9,10 @@ public class Factory {
 
 	public static Person createPerson() {
 		return new Person(1L, "Rui Barbosa", LocalDate.parse("1902-05-03"));
+	}
+
+	public static PersonDTO createPersonDTO() {
+		return new PersonDTO(null, "Rui Barbosa", LocalDate.parse("1902-05-03"));
 	}
 
 }
