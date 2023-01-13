a406564f8e53011eca9110788d4e63515606f098
Jeferson Willian Vieira Silva <jefersonwvs@gmail.com>
2023-01-13 07:55:42 -0400

    seeding file updated

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,9 +3,9 @@ INSERT INTO tbl_person(name, birth_date) VALUES('Lima Barreto', '1881-05-13');
 INSERT INTO tbl_person(name, birth_date) VALUES('José de Alencar', '1829-05-01');
 
 INSERT INTO tbl_address(street, number, zip_code, city, is_main, person_id)
-    VALUES('Rua da Alfândega', '123', '20070-005', 'Rio de Janeiro', false, 1);
+    VALUES('Rua da Alfândega', '123', '20070-005', 'Rio de Janeiro', true, 1);
 INSERT INTO tbl_address(street, number, zip_code, city, is_main, person_id)
     VALUES('Av. Pres. Wilson', '203', '20030-021', 'Rio de Janeiro', false, 1);
 
 INSERT INTO tbl_address(street, number, zip_code, city, is_main, person_id)
-    VALUES('Rua Visconde de Pirajá', '595', '20030-021', 'Rio de Janeiro', false, 2);
+    VALUES('Rua Visconde de Pirajá', '595', '20030-021', 'Rio de Janeiro', true, 2);
