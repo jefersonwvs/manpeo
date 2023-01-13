8dbd18b6aae2567770790b6599c818a7fcd0c0f8
Jeferson Willian Vieira Silva <jefersonwvs@gmail.com>
2023-01-12 21:07:25 -0400

    test: person service ut - tests for retrieve by id method

--- src/test/java/com/jefersonwvs/manpeo/services/PersonServiceTests.java
+++ src/test/java/com/jefersonwvs/manpeo/services/PersonServiceTests.java
@@ -3,6 +3,7 @@ package com.jefersonwvs.manpeo.services;
 import com.jefersonwvs.manpeo.dtos.PersonDTO;
 import com.jefersonwvs.manpeo.entities.Person;
 import com.jefersonwvs.manpeo.repositories.PersonRepository;
+import com.jefersonwvs.manpeo.services.exceptions.NotFoundException;
 import com.jefersonwvs.manpeo.utils.Factory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,6 +15,8 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Optional;
+
 @ExtendWith(SpringExtension.class)
 public class PersonServiceTests {
 
@@ -24,6 +27,7 @@ public class PersonServiceTests {
 	private PersonRepository personRepository;
 
 	private long existingId;
+	private long nonExistingId;
 
 	private Person person;
 	private PersonDTO requestDTO;
@@ -35,13 +39,19 @@ public class PersonServiceTests {
 	public void setUp() throws Exception {
 
 		existingId = 1L;
+		nonExistingId = 1000L;
 
 		person = Factory.createPerson();
 		requestDTO = Factory.createPersonDTO();
-		
+
 		// Mock do método personRepository.save()
 		Mockito.when(personRepository.save(ArgumentMatchers.any()))
 					 .thenReturn(person);
+
+		Mockito.when(personRepository.findById(existingId))
+					 .thenReturn(Optional.of(person));
+		Mockito.when(personRepository.findById(nonExistingId))
+					 .thenReturn(Optional.empty());
 
 	}
 
@@ -50,6 +60,22 @@ public class PersonServiceTests {
 		PersonDTO responseDTO = personService.create(requestDTO);
 		Assertions.assertNotNull(responseDTO);
 		Assertions.assertEquals(existingId, responseDTO.getId());
+	}
+
+	@Test
+	public void retrieveByIdShouldReturnObjectWhenIdExists() {
+		PersonDTO responseDTO = personService.retrieveById(existingId);
+		Assertions.assertNotNull(responseDTO);
+		Mockito.verify(personRepository, Mockito.times(1))
+					 .findById(existingId);
+	}
+
+	@Test
+	public void retrieveByIdShouldThrowNotFoundExceptionWhenIdDoesNotExist() {
+		Assertions.assertThrows(NotFoundException.class,
+														() -> personService.retrieveById(nonExistingId));
+		Mockito.verify(personRepository, Mockito.times(1))
+					 .findById(nonExistingId);
 	}
 
 }
